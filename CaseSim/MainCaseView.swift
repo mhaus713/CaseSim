c3879c50e51a38a1ccd1971d6ea5554f9346ff58
mhaus713 <mhaus713@gmail.com>
2016-02-04 20:55:20 -0700

    first commit

--- CaseSim/MainCaseView.swift
+++ CaseSim/MainCaseView.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainCaseView.swift
 //  CaseSim
 //
 //  Created by Michael Hollingshaus on 1/31/16.
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainCaseView: UICollectionViewController {
 
+    private let reuseIdentifier = "CaseCell"
+    private let sectionInserts = UIEdgeInserts(top: 50.0, left: 20.0, bottom: 50.0, right:20.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
