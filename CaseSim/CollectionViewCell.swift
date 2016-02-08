948cebd814dcbc1ec3d3dca28b244ba74c204238
mhaus713 <mhaus713@gmail.com>
2016-02-07 22:41:50 -0700

    made basic collection view for case simulator intro

---
+++ CaseSim/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CaseSim
+//
+//  Created by Michael Hollingshaus on 2/7/16.
+//  Copyright © 2016 LittleGiant. All rights reserved.
+//
+
+import UIKit
+
+class CaseCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var caseLabel: UILabel!
+    
+}
--- CaseSim/MainCaseView.swift
+++ CaseSim/MainCaseView.swift
@@ -8,10 +8,46 @@
 
 import UIKit
 
-class MainCaseView: UICollectionViewController {
-
-    private let reuseIdentifier = "CaseCell"
-    private let sectionInserts = UIEdgeInserts(top: 50.0, left: 20.0, bottom: 50.0, right:20.0)
+class MainCaseView: UIViewController, UICollectionViewDelegate {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    let reuseIdentifier = "caseCell"
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"]
+    
+    
+    
+    // MARK: UICollectionViewDataSource protocol
+    
+    
+    //tell collectionView how many cells to make
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    
+    //make a cell for each cell index path
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CaseCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.caseLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.yellowColor() // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
